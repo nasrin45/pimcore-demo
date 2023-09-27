5d0cc02aac524ba9228e4126127e613cc50c8994
Codilar <nasrin.k@codilar.com>
2023-09-27 12:53:54 +0530

    Initial Commit

---
+++ config/bundles.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    Pimcore\Bundle\ApplicationLoggerBundle\PimcoreApplicationLoggerBundle::class => ['all' => true],
+    Pimcore\Bundle\CustomReportsBundle\PimcoreCustomReportsBundle::class => ['all' => true],
+];
